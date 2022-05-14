88bf70bf75983f4c8f6c0c2208a97baf87c54183
Bas van Beek <74142365+bbeek@users.noreply.github.com>
2022-05-14 21:49:26 +0200

    feat: initial records demo

---
+++ Demos/Records/AccountDTO.cs
@@ -0,0 +1,133 @@
+﻿namespace Records;
+
+
+public class AccountDTO
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
+
+
+
+#region Records
+
+/*
+public record class AccountDTO(string Name, string Email) 
+{
+    //public string MutableName { get; set; } = default!;       // Always immutable?
+    
+    
+    // PrintMembers
+
+}
+*/
+
+#region Value equality
+// Show value equality ?
+
+
+#endregion
+
+#region inheritance
+/*
+public class GoldAccount : AccountDTO
+{
+    public GoldAccount(string Name, string Email) : base(Name, Email)
+    {
+    }
+}
+*/
+
+
+
+
+
+
+
+/*
+public record GoldAccount(string Name, string Email, bool IsGold) : AccountDTO(Name, Email)
+{
+}
+*/
+
+#endregion
+
+
+#region structs
+
+public struct StructExample
+{
+    public string Name { get; set; }
+    public string Email { get; set; } 
+}
+
+/*
+public record struct AccountStruct(string Name, string Email)
+{
+    public string MutableName { get; set; } = default!;
+
+};
+*/
+
+// Value types met non reflection value equality 
+
+// Usage => vervanger van Tuple<string, string, string>
+
+
+/// readonly record struct
+// Show example with MutableString
+
+
+
+
+
+
+
+
+// struct inheritance not allowed
+/*
+public struct SubStruct: StructExample
+{
+}
+*/
+
+//
+//
+//, so no record struct inheritance
+/*
+public record struct SubRecordStruct : AccountStruct
+{
+
+}
+*/
+#endregion
+
+
+#region Sealed ToString
+
+// Show with inheritance zonder sealed (en daarna met)
+/*
+public record AccountDTO(string Name, string Email)
+{
+    public string MutableName { get; set; } = default!;
+
+    
+    public sealed override string ToString()
+    {
+        return "This cannot be changed via inheritance";
+    }    
+}
+*/
+#endregion
+
+
+// Last example:
+// Mixed declaration in deconstructor
+
+// (var n, var e)
+// string n, string e
+// (n, e)
+
+// Allowed
+// (var n, e)
+#endregion
---
+++ Demos/Records/Program.cs
@@ -0,0 +1,114 @@
+﻿using Records;
+
+
+
+var account = new AccountDTO { Name = "Test", Email = "demo@example.org" };
+
+Console.WriteLine(account.Name);
+
+
+
+var copy = account;
+
+copy.Name = "Changed";
+Console.WriteLine(account.Name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+var account = new AccountDTO ("Test", "demo@example.org");
+
+Console.WriteLine(account.Name);
+
+var copy = account with { Name = "Changed"};
+
+Console.WriteLine(account);
+Console.WriteLine(copy);
+
+*/
+
+#region Immutable?
+/*
+account.MutableName = "niet immutable";
+
+Console.WriteLine(account);
+*/
+#endregion
+
+/*
+// Value equality op reference types
+var duplicate = account;
+
+Console.WriteLine(duplicate == account);
+*/
+
+
+/*
+var gold = new GoldAccount("Premium", "vip@example.org", true);
+Console.WriteLine(gold);
+*/
+
+/*
+var struct1 = new StructExample() { Name = "1", Email = "cool@demo.com" };
+var struct2 = new StructExample() { Name = "2", Email =  "super@demo.nl" };
+Console.WriteLine(struct1.Equals(struct2));
+*/
+
+#region struct
+/*
+var valueType = new AccountStruct();
+valueType.Name = "Struct";
+
+Console.WriteLine(valueType);
+*/
+// Show ILSpy value type
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// change to readonly
+#endregion
+
+
+#region With op struct
+/*
+var struct3 = struct1 with { Name = "3" };
+Console.WriteLine($"{struct3.Name} {struct3.Email}");
+*/
+#endregion
